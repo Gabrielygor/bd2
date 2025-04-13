47f89668f5e7470d1b68f2a345747fe0dbac45be
Gabrielygor <gabrielcanuto042@gmail.com>
2025-04-13 09:43:40 -0300

    feat: add exercise 4

---
+++ tarefas/t01/tarefa01-q04.sql
@@ -0,0 +1,11 @@
+SELECT f.nome,
+       f.salario,
+       d.descricao AS departamento
+FROM funcionario f
+LEFT JOIN departamento d ON f.cod_depto = d.codigo
+WHERE f.codigo NOT IN (
+    SELECT cod_gerente
+    FROM departamento
+    WHERE cod_gerente IS NOT NULL
+)
+ORDER BY f.cod_depto;
